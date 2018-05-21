fa53c2e2c457ed27f1d9b3adfe9c64b6fcf47feb
Brandon Lum <brandon.lum@ibm.com>
2018-05-21 17:31:27 -0400

    Added testcrd.go and fixed kube vendor version

--- testcrd.go
+++ testcrd.go
@@ -1,8 +1,31 @@
 package main
 
 import (
-    ctiv1 "github.ibm.com/brandon-lum/ti-keyrelease/pkg/client/clientset/versioned/typed/cti/v1"
+	"fmt"
+	ctiv1 "github.ibm.com/brandon-lum/ti-keyrelease/pkg/client/clientset/versioned/typed/cti/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
 )
-func main () {
-    _ = ctiv1
+
+func main() {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		return
+	}
+
+	client, err := ctiv1.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		return
+	}
+
+	cti, err := client.ClusterTIs("default").Get("test", meta_v1.GetOptions{})
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		return
+	}
+
+	_ = cti
+	return
 }
