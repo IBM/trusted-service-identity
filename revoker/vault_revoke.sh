f423093eeab708ffa0458342a861781d35fe7b59
Mariusz Sabath <sabath@us.ibm.com>
2018-10-19 15:17:59 -0400

    Using new values from host instead of secrets

--- revoker/vault_revoke.sh
+++ revoker/vault_revoke.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/vault login -address=$(cat /keys/address) -tls-skip-verify $(cat /keys/root-token) && \
-/vault delete -address=$(cat /keys/address) -tls-skip-verify auth/cert/certs/$1
+/vault login -address=$(cat /skeys/address) -tls-skip-verify $(cat /keys/root-token) && \
+/vault delete -address=$(cat /skeys/address) -tls-skip-verify auth/cert/certs/$1
