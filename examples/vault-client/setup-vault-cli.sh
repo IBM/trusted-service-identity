a16081fbe22a494554fa5aa4fb3efaa92496fcf7
Mariusz Sabath <sabath@us.ibm.com>
2019-03-20 11:16:41 -0400

    Adding vault client code

---
+++ examples/vault-client/setup-vault-cli.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## create help menu:
+helpme()
+{
+  cat <<HELPMEHELPME
+
+Syntax: ${0} <role>
+Where:
+  role   - name of the vault role for this login
+
+HELPMEHELPME
+}
+
+getToken()
+{
+ #validate the external parameters
+ if [ "$1" == "" ] ; then
+   export ROLE="${VAULT_ROLE}"
+ else
+   export ROLE="$1"
+ fi
+
+ export TOKEN=$(cat /jwt-tokens/token)
+ export VAULT_TOKEN=$(curl --request POST --data '{"jwt": "'"${TOKEN}"'", "role": "'"${ROLE}"'"}' ${VAULT_ADDR}/v1/auth/trusted-identity/login | jq -r '.auth.client_token')
+}
+
+# validate the arguments
+if [[ "$1" == "-?" || "$1" == "-h" || "$1" == "--help" ]] ; then
+  helpme
+#check if token exists:
+elif [ ! -f "/jwt-tokens/token" ]; then
+  echo "Token is missing '/jwt-tokens/token'"
+else
+  getToken
+fi
