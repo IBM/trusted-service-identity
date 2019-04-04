dc514c056d77457b14c010105fe859bef2847558
Mariusz Sabath <sabath@us.ibm.com>
2019-04-04 14:02:58 -0400

    testing

--- examples/vault-plugin/demo.vault-setup.sh
+++ examples/vault-plugin/demo.vault-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #ibmcloud plugin install cloud-object-storage
 export PLUGIN="vault-plugin-auth-ti-jwt"
@@ -23,6 +23,18 @@ setupVault()
   # remove any previously set VAULT_TOKEN, that overrides ROOT_TOKEN in Vault client
   export VAULT_TOKEN=
 
+
+  # vault status
+  # vault secrets enable pki
+  # # Increase the TTL by tuning the secrets engine. The default value of 30 days may
+  # # be too short, so increase it to 1 year:
+  # vault secrets tune -max-lease-ttl=8760h pki
+  # vault delete pki/root
+  # export OUT=$(vault write pki/root/generate/internal common_name=my-website.com \
+  #     ttl=8760h -format=json)
+  # echo $OUT
+  # echo $OUT | jq -r '.["data"].issuing_ca' > jwks.json
+
   # obtain the SHA256 for the plugin
   # if the deployed image has the same binary as the one on your system, use the
   # following method:
@@ -31,6 +43,7 @@ setupVault()
   # export SHA256=$(kubectl -n trusted-identity exec $(kubectl -n trusted-identity get po | grep ti-vault-| awk '{print $1}') /usr/bin/sha256sum /plugins/vault-plugin-auth-ti-jwt | cut -d' ' -f1)
 
   vault write sys/plugins/catalog/auth/vault-plugin-auth-ti-jwt sha_256="${SHA256}" command="vault-plugin-auth-ti-jwt"
+  vault read sys/plugins/catalog/auth/vault-plugin-auth-ti-jwt -format=json
   vault auth enable -path="trusted-identity" -plugin-name="vault-plugin-auth-ti-jwt" plugin
 
   export MOUNT_ACCESSOR=$(curl --header "X-Vault-Token: ${ROOT_TOKEN}"  --request GET ${VAULT_ADDR}/v1/sys/auth | jq -r '.["trusted-identity/"].accessor')
@@ -38,6 +51,10 @@ setupVault()
   # configure plugin
   # obtain JWKS-PEM (plugin-config.json) from vTPM: curl http://vtpm-service:8012/getJWKS
   curl --header "X-Vault-Token: ${ROOT_TOKEN}"  --request POST --data @jwks.json ${VAULT_ADDR}/v1/auth/trusted-identity/config
+  curl --header "X-Vault-Token: ${ROOT_TOKEN}"  --request GET ${VAULT_ADDR}/v1/auth/trusted-identity/config
+
+
+
   }
 
 if [ ! "$1" == "" ] ; then
