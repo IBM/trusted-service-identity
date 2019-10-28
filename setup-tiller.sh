7e6faed1df3f61ba03399d48d8ce56e490e00391
Mariusz Sabath <sabath@us.ibm.com>
2019-10-28 16:29:17 +0100

    Update doc to solve helm failures

---
+++ setup-tiller.sh
@@ -0,0 +1,3 @@
+kubectl --namespace kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller --upgrade
