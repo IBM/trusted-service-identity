f4787306d8ba0d7c05ee1c563d1fef85843605a8
Stefan Berger <stefanb@linux.vnet.ibm.com>
2020-03-06 09:42:20 -0500

    vtpm2-server: Prepare for using hardware TPM2
    Prepare the startup_tpm.sh script to use a hardware TPM2.
    
    I successfully simulated this by running the following in one
    terminal (using swtpm-0.3.0):
    
    	--vtpm-proxy \
    	--tpmstate dir=/tmp/vtpm2 \
    	--tpm2 \
    	--flags startup-clear \
    	--log level=0
    New TPM device: /dev/tpm1 (major/minor = 253/1)
    
    The above created /dev/tpm1.
    
    Then one can start the container using the following command line:
    
    docker run -ti --rm \
    	--env TPM_INTERFACE_TYPE=dev \
    	--env TPM_DEVICE=/dev/tpm1 \
    	--device /dev/tpm1 \
    	trustedseriviceidentity/vtpm2-server:v1.2
    
    The service should come up as expected.
    
    Signed-off-by: Stefan Berger <stefanb@linx.ibm.com>

--- components/vtpm2-server/startup_tpm.sh
+++ components/vtpm2-server/startup_tpm.sh
@@ -5,29 +5,49 @@ export TPM_SERVER_NAME=localhost
 export TPM_SERVER_TYPE=${TPM_SERVER_TYPE:-raw}
 export TPM_INTERFACE_TYPE=${TPM_INTERFACE_TYPE:-socsim}
 export TPM_COMMAND_PORT=${SWTPM_SERVER_PORT}
+export TPM_DEVICE=${TPM_DEVICE:-/dev/tpm0}
+export TPM_PERSISTENT_KEY_INDEX=${TPM_PERSISTENT_KEY_INDEX:-81230001}
 
 if [ "${TPM_INTERFACE_TYPE}" == "socsim" ]; then
 	swtpm socket \
 		--tpm2 \
-		--tpmstate dir=${STATEDIR} \
+		--tpmstate "dir=${STATEDIR}" \
 		--flags not-need-init \
 		--daemon \
 		--server type=tcp,port=${TPM_COMMAND_PORT}
+	sleep 1
+	tssstartup -c || exit 1
+	if [ -n "${VERBOSE}" ]; then
+		echo "Started swtpm and initialized it" >&2
+	fi
+else
+	if [ -n "${VERBOSE}" ]; then
+		echo "Using hardware TPM at ${TPM_DEVICE}" >&2
+	fi
+	if [ ! -c ${TPM_DEVICE} ]; then
+		echo "TPM device ${TPM_DEVICE} is not available inside the container" >&2
+	fi
 fi
 
-TPMKEYFILE=${STATEDIR}/tpm.key
-if ! [ -f ${TPMKEYFILE} ]; then
-	tssstartup -c || exit 1
-	tsscreateprimary -hi o -st -rsa &>/dev/null || exit 1
-	tssevictcontrol -hi o -ho 80000000 -hp 81000001 || exit 1
-	tssflushcontext -ha 80000000 || exit 1
-	tssreadpublic -ho 81000001 -opem ${STATEDIR}/tpmpubkey.persist.pem || exit 1
+TPMKEYFILE="${STATEDIR}/tpm.key"
+if ! [ -f "${TPMKEYFILE}" ]; then
+	# Check whether a key is already there at 'our' index
+	tssreadpublic -ho "${TPM_PERSISTENT_KEY_INDEX}" -opem "${STATEDIR}/tpmpubkey.persist.pem" &>/dev/null
+	if [ $? -ne 0 ]; then
+		# need to create the key first
+		OUTPUT=$(tsscreateprimary -hi o -st -rsa ${VERBOSE+-v} 2>&1) || { echo "${OUTPUT}" ; exit 1 ; }
+		HANDLE=$(echo "${OUTPUT}" | grep -E "^Handle" | gawk '{print $2}')
+		echo "OUPUT=$OUTPUT" >&2
+		tssevictcontrol -hi o -ho "${HANDLE}" -hp "${TPM_PERSISTENT_KEY_INDEX}" ${VERBOSE+-v} >&2 || exit 1
+		tssflushcontext -ha "${HANDLE}" ${VERBOSE+-v} >&2 || exit 1
+		tssreadpublic -ho "${TPM_PERSISTENT_KEY_INDEX}" -opem "${STATEDIR}/tpmpubkey.persist.pem" ${VERBOSE+-v} >&2 || exit 1
+	fi
 
-	create_tpm2_key -p 81000001 --rsa ${TPMKEYFILE} || exit 1
+	create_tpm2_key -p "${TPM_PERSISTENT_KEY_INDEX}" --rsa "${TPMKEYFILE}" >&2 || exit 1
 	TPMKEYURI="ibmtss2:${TPMKEYFILE}"
 	# needs to go into ${STATEDIR}/tpmkeyurl for server.py
-	echo -n ${TPMKEYURI} > ${STATEDIR}/tpmkeyurl
+	echo -n "${TPMKEYURI}" > "${STATEDIR}/tpmkeyurl"
 
-	openssl rsa -inform engine -engine tpm2 -pubout -in ${TPMKEYFILE} -out ${STATEDIR}/tpmpubkey.pem
-	openssl req -engine tpm2 -new -key ${TPMKEYFILE} -keyform engine -subj "/CN=vtpm2-jwt-server" -out ${STATEDIR}/server.csr
+	openssl rsa -inform engine -engine tpm2 -pubout -in "${TPMKEYFILE}" -out "${STATEDIR}/tpmpubkey.pem"
+	openssl req -engine tpm2 -new -key "${TPMKEYFILE}" -keyform engine -subj "/CN=vtpm2-jwt-server" -out "${STATEDIR}/server.csr"
 fi
