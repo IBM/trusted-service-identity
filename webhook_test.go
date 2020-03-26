7c7d53864d7267f9ad837d173f852e3a6af0f879
Mariusz Sabath <sabath@us.ibm.com>
2020-03-26 22:26:19 +0100

    Improved test cases

--- webhook_test.go
+++ webhook_test.go
@@ -13,6 +13,7 @@ import (
 	ctiv1 "github.com/IBM/trusted-service-identity/pkg/apis/cti/v1"
 	"github.com/nsf/jsondiff"
 	"k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 /*
@@ -37,7 +38,7 @@ func init() {
 	fmt.Println("Argument '-logLevel' is ", logLevel)
 
 	// load K8s objects and unmarshal them to test the API format
-	pod = getFakePod()
+	pod = getFakePod("tests/FakePod.json")
 	admissionRequest = getFakeAdmissionRequest()
 	admissionResponse = getFakeAdmissionResponse()
 	admissionReview = getFakeAdmissionReview()
@@ -83,6 +84,131 @@ func TestLoadInitFile(t *testing.T) {
 // 	}
 // 	t.Logf("UUID: %v", uuid)
 //}
+
+func TestIsSafe(t *testing.T) {
+	// test 1 Create OK
+	tpod := getFakePod("tests/FakeIsSafeCreateOK.json")
+	safe, err := isSafe(&tpod, "CREATE")
+	if safe && err.Error() == "" {
+		t.Logf("Testing CREATE with safe pod successful")
+	} else {
+		t.Error("Testing CREATE with safe pod failed")
+	}
+
+	// test 2 Create Error
+	tpod = getFakePod("tests/FakeIsSafeCreateError.json")
+	safe, err = isSafe(&tpod, "CREATE")
+	if !safe && err.Error() == "Using hostPath Volume with '/tsi-secure/sockets' is not allowed" {
+		t.Logf("Testing CREATE with non-safe pod successful")
+	} else {
+		t.Error("Testing CREATE with non-safe pod failed")
+	}
+
+	// test 3 Update OK
+	tpod = getFakePod("tests/FakeIsSafeUpdateOK.json")
+	safe, err = isSafe(&tpod, "UPDATE")
+	if safe && err.Error() == "" {
+		t.Logf("Testing UPDATE with safe pod successful")
+	} else {
+		t.Error("Testing UPDATE with safe pod failed")
+	}
+
+	// test 4 Update Error
+	tpod = getFakePod("tests/FakeIsSafeUpdateError.json")
+	safe, err = isSafe(&tpod, "UPDATE")
+	if !safe && err.Error() == "Attempt to modify the sidecar image to ubuntu:latest" {
+		t.Logf("Testing UPDATE with non-safe pod successful")
+	} else {
+		t.Error("Testing UPDATE with non-safe pod failed")
+	}
+
+}
+
+func TestMutationRequired(t *testing.T) {
+
+	// testing pad that should be mutated
+	meta := getFakeMetadata("tests/FakeMutationRequired.json")
+	ignoredNamespaces := []string{"kube-system", "kube-public"}
+	if mutationRequired(ignoredNamespaces, &meta) {
+		t.Logf("Testing mutating metadata successful")
+	} else {
+		t.Error("Testing mutating metadata failed")
+	}
+
+	// testing pod with  "admission.trusted.identity/inject": "false"
+	meta = getFakeMetadata("tests/FakeMutationNotRequired1.json")
+	ignoredNamespaces = []string{"kube-system", "kube-public"}
+	if !mutationRequired(ignoredNamespaces, &meta) {
+		t.Logf("Testing non-mutating metadata1 successful")
+	} else {
+		t.Error("Testing non-mutating metadata1 failed")
+	}
+
+	// testing pod with missing  "admission.trusted.identity/inject"
+	meta = getFakeMetadata("tests/FakeMutationNotRequired2.json")
+	ignoredNamespaces = []string{"kube-system", "kube-public"}
+	if !mutationRequired(ignoredNamespaces, &meta) {
+		t.Logf("Testing mutating metadata2 successful")
+	} else {
+		t.Error("Testing mutating metadata2 failed")
+	}
+}
+
+func TestUpdateAnnotation(t *testing.T) {
+
+	// test 1, standar create
+	target := getFakeAnnotation("tests/FakeUpdateAnnotationTarget.json")
+	added := getFakeAnnotation("tests/FakeUpdateAnnotation.json")
+	result := updateAnnotation(target, added)
+
+	err := validateResult(result, "tests/ExpectUpdateAnnotation.json")
+	if err != nil {
+		t.Errorf("Result updateAnnotation standard failed: %v", err)
+		return
+	}
+	t.Logf("Results updateAnnotation standard match expections")
+
+	// test 2, UPDATE to force incorrect annotations
+	target = getFakeAnnotation("tests/FakeUpdateAnnotationError.json")
+	added = getFakeAnnotation("tests/FakeUpdateAnnotation2.json")
+	result = updateAnnotation(target, added)
+
+	err = validateResult(result, "tests/ExpectUpdateAnnotation2.json")
+	if err != nil {
+		t.Errorf("Test updateAnnotation with error failed: %v", err)
+		return
+	}
+	t.Logf("Test updateAnnotation with error successful")
+
+}
+
+func TestAddContainer(t *testing.T) {
+
+	target := getFakeContainers("tests/FakeAddContainerTarget.json")
+	add := getFakeContainers("tests/FakeAddContainer.json")
+	result := addContainer(target, add, "/spec/containers")
+
+	err := validateResult(result, "tests/ExpectAddContainer.json")
+	if err != nil {
+		t.Errorf("Result AddContainer failed: %v", err)
+		return
+	}
+	t.Log("Results AddContainer match expections")
+}
+
+func TestAddVolume(t *testing.T) {
+
+	target := getFakeVolume("tests/FakeAddVolumeTarget.json")
+	add := getFakeVolume("tests/FakeAddVolume.json")
+	result := addVolume(target, add, "/spec/volumes")
+
+	err := validateResult(result, "tests/ExpectAddVolume.json")
+	if err != nil {
+		t.Errorf("Result AddVolume failed: %v", err)
+		return
+	}
+	t.Log("Results AddVolume match expections")
+}
 
 // TestMutateInitialization - tests the results of calling `mutateInitialization` in webhook
 func TestMutateInitialization(t *testing.T) {
@@ -130,11 +256,46 @@ func getContentOfTheFile(file string) string {
 	return string(dat)
 }
 
-func getFakePod() corev1.Pod {
-	s := getContentOfTheFile("tests/FakePod.json")
+func getFakePod(file string) corev1.Pod {
+	s := getContentOfTheFile(file)
 	pod := corev1.Pod{}
 	json.Unmarshal([]byte(s), &pod)
 	return pod
+}
+
+func getFakeContainers(file string) []corev1.Container {
+	s := getContentOfTheFile(file)
+	c := []corev1.Container{}
+	json.Unmarshal([]byte(s), &c)
+	return c
+}
+
+func getFakeVolume(file string) []corev1.Volume {
+	s := getContentOfTheFile(file)
+	vol := []corev1.Volume{}
+	json.Unmarshal([]byte(s), &vol)
+	return vol
+}
+
+func getFakeVolumeMount(file string) []corev1.VolumeMount {
+	s := getContentOfTheFile(file)
+	vol := []corev1.VolumeMount{}
+	json.Unmarshal([]byte(s), &vol)
+	return vol
+}
+
+func getFakeMetadata(file string) metav1.ObjectMeta {
+	s := getContentOfTheFile(file)
+	meta := metav1.ObjectMeta{}
+	json.Unmarshal([]byte(s), &meta)
+	return meta
+}
+
+func getFakeAnnotation(file string) map[string]string {
+	s := getContentOfTheFile(file)
+	ann := make(map[string]string)
+	json.Unmarshal([]byte(s), &ann)
+	return ann
 }
 
 func getFakeAdmissionRequest() v1beta1.AdmissionRequest {
