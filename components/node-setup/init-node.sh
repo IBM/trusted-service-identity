74f717fa1acf0046be65414dd56a98001fbe9052
Mariusz Sabath <sabath@us.ibm.com>
2020-05-06 14:24:54 +0200

    Always create openssl.cnf on worker host
    Both JSS and VTPM need openssl.cnf file to setup node identity in CA

--- components/node-setup/init-node.sh
+++ components/node-setup/init-node.sh
@@ -9,12 +9,23 @@ AUDIT_LOG=/host/logs/tsi-audit.log
 PRIV_KEY=${PRIVATEDIR}/private.key
 SERV_CSR=${PRIVATEDIR}/server.csr
 X5C=${PRIVATEDIR}/x5c
+SSLCONF=${PRIVATEDIR}/openssl.cnf
 
 # a handy function to format the audit log
 logme() {
 echo "$1"
 now=$(date +"%Y-%m-%d.%H:%M:%S")
 echo "$now,$1" >> ${AUDIT_LOG}
+}
+
+# this function ends the operation and waits forever, so the container is not
+# recreated
+end() {
+  # end of the audit log
+  logme "end of audit record. Waiting forever now..."
+
+  # keep the pod running
+  tail -f /dev/null
 }
 
 # output some info about the pod processing these operations
@@ -27,6 +38,31 @@ if [ "$RESETALL" == "true" ]; then
   rm -rf "${PRIVATEDIR}"
   #rm ${PRIV_KEY} ${SERV_CSR}
   logme "full reset performed"
+fi
+
+# openssl.cnf contains cluster idenity information and must be always created
+# even if private keys are not created (e.g. to be used by VTPM2)
+if [ "$CLUSTER_REGION" == "" ] || [ "$CLUSTER_NAME" == "" ]; then
+  logme "Env. variables CLUSTER_REGION and CLUSTER_NAME must be set. Terminating..."
+  end
+else
+  cat > ${SSLCONF} << EOF
+[req]
+req_extensions = v3_req
+
+[v3_req]
+subjectAltName= @alt_names
+
+[alt_names]
+URI.1 = TSI:cluster-name:$CLUSTER_NAME
+URI.2 = TSI:cluster-region:$CLUSTER_REGION
+# To assert additional claims about this intermediate CA
+# add new lines in the following format:
+# URI.x = TSI:<claim>
+# where x is a next sequencial number and claim is
+# a `key:value` pair. For example:
+# URI.3 = TSI:datacenter:fra02
+EOF
 fi
 
 # reset the private key and all the other secure stuff
@@ -53,42 +89,12 @@ fi
 # create a private key
 if [ "$RESETALL" == "true" ]; then
   if ! [ -f "${PRIV_KEY}" ]; then
-
-    if [ "$CLUSTER_REGION" == "" ] || [ "$CLUSTER_NAME" == "" ]; then
-      logme "Env. variables CLUSTER_REGION and CLUSTER_NAME must be set. Terminating..."
-    else
-      cat > "openssl.cnf" << EOF
-[req]
-req_extensions = v3_req
-
-[v3_req]
-subjectAltName= @alt_names
-
-[alt_names]
-URI.1 = TSI:cluster-name:$CLUSTER_NAME
-URI.2 = TSI:cluster-region:$CLUSTER_REGION
-# To assert additional claims about this intermediate CA
-# add new lines in the following format:
-# URI.x = TSI:<claim>
-# where x is a next sequencial number and claim is
-# a `key:value` pair. For example:
-# URI.3 = TSI:datacenter:fra02
-EOF
-
       openssl genrsa -out "${PRIV_KEY}" 2048
       openssl req -new -sha256 -key "${PRIV_KEY}" -out "${SERV_CSR}" -subj "/CN=jss-jwt-server" -reqexts v3_req -config <(cat /etc/ssl/openssl.cnf openssl.cnf)
       logme "private key ${PRIV_KEY} and ${SERV_CSR} created"
       logme "for cluster-name: $CLUSTER_NAME and cluster-region:$CLUSTER_REGION"
-    fi
-
-  else
-   logme "private key ${PRIV_KEY} and ${SERV_CSR} already exist. Do nothing"
-  fi
+   else
+     logme "private key ${PRIV_KEY} and ${SERV_CSR} already exist. Do nothing"
+   fi
 fi
-
-
-# end of the audit log
-logme "end of audit record"
-
-# keep the pod running
-tail -f /dev/null
+end
