b27d51996246d7f2a92127b504faeeab96f2b298
Mariusz Sabath <sabath@us.ibm.com>
2020-05-29 23:04:12 +0200

    Code cleanup

--- components/node-setup/secret-maker.sh
+++ components/node-setup/secret-maker.sh
@@ -29,7 +29,7 @@ cat << EOF
 # they must be in "key=value" format
 # For more info see:
 # https://github.com/IBM/trusted-service-identity/examples/vault/README.md#secrets
-export SECRET_VALUE="secret="
+export SECRET_VALUE="secret=xxx"
 
 # To access vault, obtain the Vault Client from (...)
 #  and define ROOT_TOKEN and VAULT_ADDR env. variables:
@@ -70,42 +70,14 @@ buildSecrets()
   local SECNAME=$1
   local CONSTR=$2
 
-  # Then parse the response to get other attributes associated
-  # with this token.
-  # Doublequotes required when the key name contains '-'
-  # REGION=$(echo $RESP | jq -r '.auth.metadata."region"')
-  # CLUSTER=$(echo $RESP | jq -r '.auth.metadata."cluster-name"')
-  # IMGSHA=$(echo $RESP | jq -r '.auth.metadata.images')
-  # NS=$(echo $RESP | jq -r '.auth.metadata.namespace')
-  #
-  # echo "Getting $SECNAME from Vault $VAULT_PATH and output to $LOCPATH"
-  # if [ "$VAULT_PATH" == "secret/tsi-rcni" ]; then
-  #   CMD="vault kv get -format=json ${VAULT_PATH}/${REGION}/${CLUSTER}/${NS}/${IMGSHA}/${SECNAME}"
-  # elif [ "$VAULT_PATH" == "secret/tsi-r" ]; then
-  #   CMD="vault kv get -format=json ${VAULT_PATH}/${REGION}/${SECNAME}"
-  # elif [ "$VAULT_PATH" == "secret/tsi-rcn" ]; then
-  #   CMD="vault kv get -format=json ${VAULT_PATH}/${REGION}/${CLUSTER}/${NS}/${SECNAME}"
-  # else
-  #   echo "Unknown Vault path value!"
-  #   rm -rf "${LOCPATHDIR}/${LOCPATH}/${SECNAME}"
-  #   return 1
-  # fi
-
   case $CONSTR in
       "region")
           echo "# using policy $CONSTR"
-          # PL="tsi-ri"
-          # REGION=`echo $CLAIMS |jq -r '."region"'`
-          # IMG=`echo $CLAIMS |jq -r '."images"'`
           echo "vault kv put secret/tsi-r/${REGION}/${SECNAME} ${SECRET_VALUE}"
           POLICIES+=('tsi-r')
           ;;
       "region,images")
           echo "# using policy $CONSTR"
-          # PL="tsi-r"
-          # REGION=`echo $CLAIMS |jq -r '."region"'`
-          # CLUSTER=`echo $CLAIMS |jq -r '."cluster-name"'`
-          # IMG=`echo $CLAIMS |jq -r '."images"'`
           echo "vault kv put secret/tsi-ri/${REGION}/${IMGSHA}/${SECNAME} ${SECRET_VALUE}"
           POLICIES+=('tsi-ri')
           ;;
@@ -113,46 +85,20 @@ buildSecrets()
           echo "# using policy $CONSTR"
           ROLE="tsi-role-rcn"
           VAULT_PATH="secret/tsi-rcn"
-          # PL="tsi-r"
-          # REGION=`echo $CLAIMS |jq -r '."region"'`
-          # CLUSTER=`echo $CLAIMS |jq -r '."cluster-name"'`
-          # IMG=`echo $CLAIMS |jq -r '."images"'`
           echo "vault kv put secret/tsi-rcn/${REGION}/${CLUSTER}/${NS}/${SECNAME} ${SECRET_VALUE}"
           POLICIES+=('tsi-rcn')
           ;;
       "region,cluster-name,namespace,images")
           echo "# using policy $CONSTR"
-          # PL="tsi-rcni"
-          # REGION=`echo $CLAIMS |jq -r '."region"'`
-          # CLUSTER=`echo $CLAIMS |jq -r '."cluster-name"'`
-          # NS=`echo $CLAIMS |jq -r '."namespace"'`
-          # IMG=`echo $CLAIMS |jq -r '."images"'`
           echo "vault kv put secret/tsi-rcni/${REGION}/${CLUSTER}/${NS}/${IMGSHA}/${SECNAME} ${SECRET_VALUE}"
           POLICIES+=('tsi-rcni')
           ;;
       *) echo "# ERROR: invalid constrains requested: ${CONSTR}"
          ;;
   esac
-
 }
 
 #### End of functions
-
-# {
-#   "cluster-name": "my-cluster-name",
-#   "region": "eu-de",
-#   "exp": 1590494405,
-#   "iat": 1590494345,
-#   "images": "30beed0665d9cb4df616cca84ef2c06d2323e02869fcca8bbfbf0d8c5a3987cc",
-#   "images-names": "ubuntu@sha256:250cc6f3f3ffc5cdaa9d8f4946ac79821aafb4d3afc93928f0de9336eba21aa4",
-#   "iss": "wsched@us.ibm.com",
-#   "machineid": "b46e165c32d342d9896d9eeb43c4d5dd",
-#   "namespace": "test",
-#   "pod": "myubuntu-6756d665bc-cb49l",
-#   "sub": "wsched@us.ibm.com"
-# }
-# root@myubuntu-6756d665bc-cb49l:/# cat jwt/token | cut -d"." -f2 |base64 --decode |jq -r '.images'
-# 30beed0665d9cb4df616cca84ef2c06d2323e02869fcca8bbfbf0d8c5a3987cc
 
 # validate the arguments
 if [[ "$1" == "-?" || "$1" == "-h" || "$1" == "--help" || "$2" == "" ]] ; then
@@ -166,13 +112,11 @@ POD_JSON="$2"
 
 ### Get Cluter Information
 
-# extract cluster and region info from K8s
+# extract cluster and region info from provided data
 CLYM1="/tmp/cl1.$$"
-#kubectl get cm -n kube-system cluster-info -o yaml > ${CLYM1}
 cat ${CLUSTER_YAML} > ${CLYM1}
 CLJS1=$(yq r -j ${CLYM1} |jq -r '.data."cluster-config.json"')
 rm "${CLYM1}"
-#echo $CLJS1
 CLUSTER=$(echo "$CLJS1" | jq -r '.name')
 DC=$(echo "$CLJS1" | jq -r '.datacenter')
 
@@ -184,27 +128,8 @@ echo "# Cluster: $CLUSTER DataCenter: $DC Region: $REGION"
 
 # Get Pod information
 
-# extract the deployment information from the deployment file
-# DEPLOY=$(cat ${POD_JSON} |jq)
-# DEPLOY=$(kubectl create -f ${FILE} -n ${NS} --dry-run=true -ojson |jq)
-
-#  OLD CODE... >>>
-
-
-
-#secrets=$(echo $DEPLOY | jq -r '.spec.template.metadata.annotations."tsi.secrets"')
-#echo $DEPLOY | jq -r '.spec.template.metadata.annotations'
-
-#echo "$secrets"
-
-
-
-
-
 # getting the secrets annotations require a bit more work
 TMPTSI="/tmp/tsi.$$"
-# kubectl create -f ${FILE} -n ${NS} --dry-run=true -oyaml > ${TMPTSI}.1
-
 cat ${POD_JSON} > ${TMPTSI}.1
 yq r -j ${TMPTSI}.1 |jq -r '.spec.template.metadata.annotations."tsi.secrets"' > ${TMPTSI}.2
 
@@ -241,16 +166,8 @@ else
   echo "# Set 'admission.trusted.identity/inject' annotation to 'true'"
   exit 1
 fi
-
-
-
 echo "# NS=$NS, IMG: $IMG, IMGSHA: $IMGSHA"
 
-
-
-
-#JSON1=$(yq r -j /tmp/tsi.1)
-#echo $JSON1 | jq -r '.spec.template.metadata.annotations."tsi.secrets"' > /tmp/tsi.2
 JSON=$(yq r -j ${TMPTSI}.2)
 rm ${TMPTSI}.1 ${TMPTSI}.2
 
@@ -275,12 +192,8 @@ for row in $(echo "${JSON}" | jq -c '.[]' ); do
   else
     echo "# ERROR: invalid local-path value: $LOCPATH"
   fi
-
-
-
 done
 
-#echo "# ${POLICIES[@]}"
 # remove duplicated policies:
 sorted_unique_ids=($(echo "${POLICIES[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 echo "# this deployment uses the following policies/paths: "
