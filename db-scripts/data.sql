deac54de5944be2ac58f32b840386b714aeeeaff
Florentin Vintila <florentin.vintila@gmail.com>
2021-10-05 17:20:53 +0300

    finished yaml files

--- db-scripts/data.sql
+++ db-scripts/data.sql
@@ -1,6 +1,6 @@
 USE testdb;
 
-insert into movie(id, name, year, director, genre) values 
+insert into MOVIE(id, name, year, director, genre) values 
 (1, "Bruce Almighty", 2003, "Tom Shaydac", "Comedy"),
 (2, "The Godfather", 1972, "Francis Ford Coppola", "Crime")
 ;
