992d1b7aa5e90f2cea13140ab9908d508091d8be
Mariusz Sabath <mrsabath@gmail.com>
2021-12-20 13:57:08 -0500

    Post-migration cleanup
    Signed-off-by: Mariusz Sabath <mrsabath@gmail.com>

--- db-scripts/data.sql
+++
@@ -1,6 +0,0 @@
-USE testdb;
-
-insert into MOVIE(id, name, year, director, genre) values 
-(1, "Bruce Almighty", 2003, "Tom Shaydac", "Comedy"),
-(2, "The Godfather", 1972, "Francis Ford Coppola", "Crime")
-;
