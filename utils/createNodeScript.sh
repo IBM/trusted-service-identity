3f9d847629cde435e6aac8b69e6b2d318bbc0b71
Mariusz Sabath <mrsabath@gmail.com>
2022-04-26 19:31:36 -0400

    Script updates
    Signed-off-by: Mariusz Sabath <mrsabath@gmail.com>

--- utils/createNodeScript.sh
+++ utils/createNodeScript.sh
@@ -13,68 +13,70 @@ if [[ "$2" != "" ]] ; then
 else
   KEYS="../x509"
 fi
-FILE=$NODE.sh
-TARGET_DIR="/target/run/spire/x509"
+
+SCRIPTS="scripts"
+FILE=${SCRIPTS}/${NODE}.sh
 TEMP_DIR="/tmp/ca"
+TARGET_DIR="/target/run/spire/x509"
 
-echo "#!/bin/bash -x" > $FILE
-chmod 755 $FILE
+echo "#!/bin/bash" > ${FILE}
+chmod 755 ${FILE}
 
-echo "mkdir -p $TEMP_DIR" >> $FILE
-echo "cd $TEMP_DIR" >> $FILE
-echo "mkdir certs crl newcerts private" >> $FILE
-echo "chmod 700 private" >> $FILE
-echo "touch index.txt" >> $FILE
-echo "echo 1000 > serial" >> $FILE
-echo "cd -" >> $FILE
+echo "mkdir -p $SCRIPTS" >> ${FILE}
+echo "mkdir -p ${TEMP_DIR}" >> ${FILE}
+echo "cd ${TEMP_DIR}" >> ${FILE}
+echo "mkdir certs crl newcerts private" >> ${FILE}
+echo "chmod 700 private" >> ${FILE}
+echo "touch index.txt" >> ${FILE}
+echo "echo 1000 > serial" >> ${FILE}
+echo "cd -" >> ${FILE}
 
-echo "cat > $TEMP_DIR/intermediate.cert.pem <<EOF" >> $FILE
-if [ -f $KEYS/intermediate.cert.pem ]; then
-  cat $KEYS/intermediate.cert.pem >> $FILE
-  echo "EOF" >> $FILE
-  echo " " >> $FILE
+echo "cat > ${TEMP_DIR}/intermediate.cert.pem <<EOF" >> ${FILE}
+if [ -f ${KEYS}/intermediate.cert.pem ]; then
+  cat ${KEYS}/intermediate.cert.pem >> ${FILE}
+  echo "EOF" >> ${FILE}
+  echo " " >> ${FILE}
 else
-  echo "Error! Missing file $KEYS/intermediate.cert.pem"
+  echo "Error! Missing file ${KEYS}/intermediate.cert.pem"
   exit 1
 fi
 
-if [ -f $KEYS/intermediate.key.pem ]; then
-  echo "cat > $TEMP_DIR/intermediate.key.pem <<EOF" >> $FILE
-  cat $KEYS/intermediate.key.pem >> $FILE
-  echo "EOF" >> $FILE
-  echo " " >> $FILE
+if [ -f ${KEYS}/intermediate.key.pem ]; then
+  echo "cat > ${TEMP_DIR}/intermediate.key.pem <<EOF" >> ${FILE}
+  cat ${KEYS}/intermediate.key.pem >> ${FILE}
+  echo "EOF" >> ${FILE}
+  echo " " >> ${FILE}
 else
-  echo "Error! Missing file $KEYS/intermediate.key.pem"
+  echo "Error! Missing file ${KEYS}/intermediate.key.pem"
   exit 1
 fi
 
-echo "cat > $TEMP_DIR/intermediate-openssl.cnf <<EOF" >> $FILE
-cat conf/intermediate-config.txt >> $FILE
-echo "EOF" >> $FILE
-echo " " >> $FILE
+echo "cat > ${TEMP_DIR}/intermediate-openssl.cnf <<EOF" >> ${FILE}
+cat conf/intermediate-config.txt >> ${FILE}
+echo "EOF" >> ${FILE}
+echo " " >> ${FILE}
 
-echo "openssl genrsa -out $TEMP_DIR/node.key.pem 2048" >> $FILE
-echo "chmod 400 $TEMP_DIR/node.key.pem" >> $FILE
+echo "openssl genrsa -out ${TEMP_DIR}/node.key.pem 2048" >> ${FILE}
+echo "chmod 400 ${TEMP_DIR}/node.key.pem" >> ${FILE}
 
-echo 'SUBJ="/C=US/ST=CA/O=MyOrg, Inc./CN='"$NODE"'"' >> $FILE
+echo 'SUBJ="/C=US/ST=CA/O=MyOrg, Inc./CN='"$NODE"'"' >> ${FILE}
 
-echo "openssl req -new -sha256 -key $TEMP_DIR/node.key.pem \\" >> $FILE
-echo ' -subj "${SUBJ}"'" -out $TEMP_DIR/node.csr \\" >> $FILE
-echo " -config $TEMP_DIR/intermediate-openssl.cnf 2>/dev/null" >> $FILE
+echo "openssl req -new -sha256 -key ${TEMP_DIR}/node.key.pem \\" >> ${FILE}
+echo ' -subj "${SUBJ}"'" -out ${TEMP_DIR}/node.csr \\" >> ${FILE}
+echo " -config ${TEMP_DIR}/intermediate-openssl.cnf 2>/dev/null" >> ${FILE}
 
-echo "openssl ca -batch -config $TEMP_DIR/intermediate-openssl.cnf \\" >> $FILE
-echo "    -extensions server_cert -days 375 -notext -md sha256 \\" >> $FILE
-echo "    -in $TEMP_DIR/node.csr \\" >> $FILE
-echo "    -out $TEMP_DIR/node.cert.pem 2>/dev/null" >> $FILE
-echo "chmod 444 $TEMP_DIR/node.cert.pem" >> $FILE
+echo "openssl ca -batch -config ${TEMP_DIR}/intermediate-openssl.cnf \\" >> ${FILE}
+echo "    -extensions server_cert -days 375 -notext -md sha256 \\" >> ${FILE}
+echo "    -in ${TEMP_DIR}/node.csr \\" >> ${FILE}
+echo "    -out ${TEMP_DIR}/node.cert.pem 2>/dev/null" >> ${FILE}
+echo "chmod 444 ${TEMP_DIR}/node.cert.pem" >> ${FILE}
 
-echo "" >> $FILE
-echo "# cleanup:" >> $FILE
-echo "mkdir -p $TARGET_DIR" >> $FILE
-echo "cat $TEMP_DIR/node.cert.pem \\" >> $FILE
-echo "   $TEMP_DIR/intermediate.cert.pem > $TARGET_DIR/node-bundle.cert.pem" >> $FILE
-echo "mv $TEMP_DIR/node.key.pem $TARGET_DIR/" >> $FILE
-echo "rm -rf $TEMP_DIR/" >> $FILE
-echo "" >> $FILE
-echo "" >> $FILE
-echo "" >> $FILE
+echo "" >> ${FILE}
+echo "# cleanup:" >> ${FILE}
+echo "mkdir -p ${TARGET_DIR}" >> ${FILE}
+echo "cat ${TEMP_DIR}/node.cert.pem \\" >> ${FILE}
+echo "   ${TEMP_DIR}/intermediate.cert.pem > ${TARGET_DIR}/node-bundle.cert.pem" >> ${FILE}
+echo "mv ${TEMP_DIR}/node.key.pem ${TARGET_DIR}/" >> ${FILE}
+echo "rm -rf ${TEMP_DIR}/" >> ${FILE}
+echo "rm -rf $SCRIPTS/" >> ${FILE}
+echo "" >> ${FILE}
